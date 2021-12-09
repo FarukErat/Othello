653fbacedc48545ba5913556536ecb64a36c0172
FarukErat <92527106+FarukErat@users.noreply.github.com>
2021-12-09 22:53:27 +0300

    4

--- flipper.h
+++ flipper.h
@@ -24,42 +24,46 @@ void flipper(int col, int row, char turn)
 
         for (m = 1; (i >= 0 && i <= 7) && (j >= 0 && j <= 7); m++)
         {
-            i = col - 1 + m * dirs[dir][0]; //multiplying by m
+            i = col - 1 + m * dirs[dir][0]; //multiplying by 'm'
             j = row - 1 + m * dirs[dir][1]; //to move along the directions
 
             if ((board[i][j] == '-') || (board[col - 1 + dirs[dir][0]][row - 1 + dirs[dir][1]] == turn))
+                // if the calculated square is empty ('-'), break
+                // or
+                // if the square which is by the chosen square
+                // is in the same color as players, break
                 break;
 
-            if (board[i][j] == turn)
+            if (board[i][j] == turn) // when a square in the same color as players is found
             {
-                k = col - 1 + dirs[dir][0];
+                k = col - 1 + dirs[dir][0]; //k and l are assigned the number along the legal direction
                 l = row - 1 + dirs[dir][1];
 
-                if (dirs[dir][0] == 0)
+                if (dirs[dir][0] == 0) // assigning the squares, when the direction is upward or downward
                 {
-                    while (l != j)
+                    while (l != j) //assign till j
                     {
                         board[k][l] = turn;
-                        l += dirs[dir][1];
+                        l += dirs[dir][1]; //incrase the number by the delta
                     }
                 }
 
-                if (dirs[dir][1] == 0)
+                if (dirs[dir][1] == 0) // assigning the squares, when the direction is left or right
                 {
-                    while (k != i)
+                    while (k != i) //assign till i
                     {
                         board[k][l] = turn;
-                        k += dirs[dir][0];
+                        k += dirs[dir][0]; //incrase the number by the delta
                     }
                 }
 
-                else
+                else  // assigning the squares, when the direction is cross
                 {
-                    while (k != i && l != j)
+                    while (k != i && l != j) //assign till i or j
                     {
                         board[k][l] = turn;
 
-                        k += dirs[dir][0];
+                        k += dirs[dir][0]; //incrase the numbers by the delta
                         l += dirs[dir][1];
                     }
                 }

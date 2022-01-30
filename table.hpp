0a7bf3516ee389be2b46ebc61ee2f1bdcaba5707
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-01-30 19:39:38 +0300

    Update table.hpp

--- table.hpp
+++ table.hpp
@@ -1,47 +1,5 @@
 #pragma once
 
-#include <ctime>     //to generate random numbers for cpu response
-#include <conio.h>   //for getch to keep the terminal still to see the result
-#include <cstdlib>   //for rand()
-
-// custom includes are at the bottom of the file
-
-#ifdef _WIN32
-#include <Windows.h> //for Sleep() and system("cls")
-void sleep(unsigned sec){
-    Sleep(sec * 1000);
-}
-#else
-#include <unistd.h>
-void sleep(unsigned sec){
-    usleep(sec * 1000);
-}
-#endif
-
-#define BLACK 'B'
-#define WHITE 'W'
-#define EMPTY '-'
-//the size is adjustable, but the size must be even and grater than 3
-#define SIZE 8
-
-using namespace std;
-
-struct coor
-{
-	int col;
-	int row;
-};
-
-// Define all the possible directions in which a player's move can flip
-// their adversary's tiles as constant (0 – the current row/column, 
-// +1 – the next row/column, -1 – the previous row/column)
-int moveDirs[8][2] =
-{
-    {-1, -1},   {-1, 0},    {-1, 1},
-    {0, -1},                {0, 1},
-    {1, -1},    {1, 0},     {1, 1}
-};
-
 class Table
 {
 public:
@@ -65,38 +23,3 @@ public:
     coor userPlays();
     coor cpuPlays();
 };
-
-Table::Table()
-{
-    this->turn = BLACK;
-
-    // creating a blank board
-    for (int row = 0; row < SIZE; row++)
-        for (int col = 0; col < SIZE; col++)
-            this->board[row][col] = EMPTY;
-
-    // adding the initial squares
-    this->board[SIZE/2 - 1][SIZE/2]     = BLACK;
-    this->board[SIZE/2][SIZE/2 - 1]     = BLACK;
-    this->board[SIZE/2 - 1][SIZE/2 - 1] = WHITE;
-    this->board[SIZE/2][SIZE/2]         = WHITE;
-}
-
-Table::~Table()
-{
-    system("cls");
-    cout << endl
-    << "The program has ended." << endl
-    << "Have a good day!"
-    << endl;
-}
-
-#include "is_legal.hpp"
-#include "is_on_board.hpp"
-#include "has_tile_to_flip.hpp"
-#include "print_board.hpp"
-#include "user_plays.hpp"
-#include "cpu_plays.hpp"
-#include "flip_tiles.hpp"
-#include "game.hpp"
-#include "settings.hpp"

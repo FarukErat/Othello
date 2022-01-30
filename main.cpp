b75f22d996af4af61ad2259a0cd7d4e864195eea
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-01-30 18:09:23 +0300

    include update

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-#include <ctime>     //to generate random numbers for cpu response
-#include <conio.h>   //for getch to keep the terminal still to see the result
-#include <cstdlib>   //for rand()
 
-#ifdef _WIN32
-#include <Windows.h> //for Sleep() and system("cls")
-void sleep(unsigned sec){
-    Sleep(sec * 1000);
-}
-#else
-#include <unistd.h>
-void sleep(unsigned sec){
-    usleep(sec * 1000);
-}
-#endif
-
-#include "libraries.h"
+#include "table.h"
 
 using namespace std;
 

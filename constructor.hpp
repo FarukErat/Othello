77eeff946be2d28465e5442a834e71953e6eef61
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-01-30 19:38:40 +0300

    Add files via upload

---
+++ constructor.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "table.hpp"
+
+Table::Table()
+{
+    this->turn = BLACK;
+
+    // creating a blank board
+    for (int row = 0; row < SIZE; row++)
+        for (int col = 0; col < SIZE; col++)
+            this->board[row][col] = EMPTY;
+
+    // adding the initial squares
+    this->board[SIZE/2 - 1][SIZE/2]     = BLACK;
+    this->board[SIZE/2][SIZE/2 - 1]     = BLACK;
+    this->board[SIZE/2 - 1][SIZE/2 - 1] = WHITE;
+    this->board[SIZE/2][SIZE/2]         = WHITE;
+}

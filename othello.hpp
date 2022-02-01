c68f6e768a1ffdf95bc1c24826532fac6c562afe
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-01 21:26:35 +0300

    dynamic-sized moves list

--- othello.hpp
+++ othello.hpp
@@ -3,6 +3,7 @@
 #include <ctime>     //to generate random numbers for cpu response
 #include <conio.h>   //for getch to keep the terminal still to see the result
 #include <cstdlib>   //for rand()
+#include <vector>    //for vector of valid moves of cpu
 
 #ifdef _WIN32
 #include <Windows.h> //for Sleep() and system("cls")

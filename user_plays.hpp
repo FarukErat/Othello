17c5c14c453b0f5dfe9f75edc570565a58785a86
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-02 21:52:40 +0300

    Update user_plays.hpp

--- user_plays.hpp
+++ user_plays.hpp
@@ -2,31 +2,25 @@
 
 #include "table.hpp"
 /**
+ * @brief this takes coordinates, if they are not legal the user is warned and asked to enter again
  * @param nothing
  * @return coordinates of the square the user choose
  */
 coor Table::userPlays()
-// this takes coordinates, if they are not legal the user is warned and asked to enter again
 {
     coor c;
-    bool flag;
-
     do
     {
-        flag = false;
-
         cout << "row and col\n";
-        cin  >> c.row >> c.col;
+        cin >> c.row >> c.col;
 
         c.row--;
         c.col--;
+        for (auto dir : moveDirs)
+            if (isLegal(c, dir))
+                return c;
 
-        if (!isLegal(c))
-        {
-            cout << "\nInvalid position!!!" << endl
-                 << "Enter again\n\n";
-            flag = true;
-        }
-    } while (flag);
-    return c;
+        cout << "\nInvalid position!!!" << endl
+             << "Enter again\n\n";
+    } while (true);
 }

02f58540286bb99b7c07cd49054392aaca184c8e
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-02 21:48:18 +0300

    Update has_tile_to_flip.hpp

--- has_tile_to_flip.hpp
+++ has_tile_to_flip.hpp
@@ -8,16 +8,17 @@
 bool Table::hasTileToFlip()
 // checks if a side has tile to flip
 {
-    coor c;
-    // this checks if there is any legal move for only one side
+	coor c;
+	// this checks if there is any legal move for only one side
 	for (int row = 0; row < SIZE; row++)
 	{
 		for (int col = 0; col < SIZE; col++)
 		{
-            c.row = row;
-            c.col = col;
-			if (isLegal(c))
-				return true;
+			c.row = row;
+			c.col = col;
+			for (auto dir : moveDirs)
+				if (isLegal(c, dir))
+					return true;
 		}
 	}
 	return false;

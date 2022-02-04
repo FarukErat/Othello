d8e6ccab9c63182f50c25206b4ba0bc748e0517b
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-04 21:41:39 +0300

    Update table.hpp

--- table.hpp
+++ table.hpp
@@ -1,19 +1,20 @@
 #pragma once
 
-//the size is adjustable, but the size must be even and grater than 3
+// the size is adjustable, but the size must be even and grater than 3
 #define SIZE 8
 #define BLACK 'B'
 #define WHITE 'W'
 #define EMPTY '-'
+#define LEGAL '*'
 
 struct coor
 {
-	int col;
-	int row;
+    int col;
+    int row;
 };
 
 // Define all the possible directions in which a player's move can flip
-// their adversary's tiles as constant (0 – the current row/column, 
+// their adversary's tiles as constant (0 – the current row/column,
 // +1 – the next row/column, -1 – the previous row/column)
 int moveDirs[8][2] =
 {
@@ -25,6 +26,7 @@ int moveDirs[8][2] =
 class Table
 {
 public:
+    bool marking;
     char board[SIZE][SIZE];
     int choice;
     char turn;
@@ -33,15 +35,16 @@ public:
 
     Table();
     ~Table();
-    
+
+    bool hasTileToFlip();
     bool isLegal(coor);
     bool isOnBoard(coor);
-    bool hasTileToFlip();
 
     void settings();
     void game();
     void printBoard();
     void flipTiles(coor);
+    void marker();
 
     coor userPlays();
     coor cpuPlays();

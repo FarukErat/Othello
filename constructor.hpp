8718c3e560b2ddff6004c5aa66fc5fa73a2ef766
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-07 00:00:18 +0300

    Update constructor.hpp

--- constructor.hpp
+++ constructor.hpp
@@ -5,17 +5,17 @@
 Table::Table()
 {
     // player black starts the game
-    this->turn = BLACK;
-    this->oponent = WHITE;
+    setTurn(BLACK);
+    setOponent(WHITE);
 
     // creating a blank board
     for (int row = 0; row < SIZE; row++)
         for (int col = 0; col < SIZE; col++)
-            this->board[row][col] = EMPTY;
+            setBoard(row, col, EMPTY);
 
     // adding the initial squares
-    this->board[SIZE/2 - 1][SIZE/2]     = BLACK;
-    this->board[SIZE/2][SIZE/2 - 1]     = BLACK;
-    this->board[SIZE/2 - 1][SIZE/2 - 1] = WHITE;
-    this->board[SIZE/2][SIZE/2]         = WHITE;
+    setBoard(SIZE/2 - 1, SIZE/2,     BLACK);
+    setBoard(SIZE/2, SIZE/2 - 1,     BLACK);
+    setBoard(SIZE/2 - 1, SIZE/2 - 1, WHITE);
+    setBoard(SIZE/2, SIZE/2,         WHITE);
 }

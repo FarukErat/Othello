256d3c950a030ea4db5c1557b82e03cc60ec2ebc
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-09 23:23:06 +0300

    unnecessary include guards removed
    I had added some include guards for built-in libraries to prevent multi-includes as if they don't have one :D, but anyway, I removed them

--- othello.hpp
+++ othello.hpp
@@ -16,34 +16,15 @@ void sleep(float sec)
 }
 #endif
 
-#ifndef _IOSTREAM_
-#define _IOSTREAM_
 #include <iostream>
-#endif
-
 // to generate random numbers for cpu response
-#ifndef _CTIME_
-#define _CTIME_
 #include <ctime>
-#endif
-
 // for getch to keep the terminal still to see the result
-#ifndef _INC_CONIO
-#define _INC_CONIO
 #include <conio.h>
-#endif
-
 // for rand()
-#ifndef _CSTDLIB_
-#define _CSTDLIB_
 #include <cstdlib>
-#endif
-
 // for vector of valid moves of cpu
-#ifndef _VECTOR_
-#define _VECTOR_
 #include <vector>
-#endif
 
 using std::cin;
 using std::cout;

4556e77dcfaafe458382e9b5b35407285e6f393e
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-06 16:08:22 +0300

    Update check_file.hpp

--- check_file.hpp
+++ check_file.hpp
@@ -23,28 +23,11 @@ bool Table::checkFile()
         // legal tiles are flipped here
         flipTiles(c);
         // switching the side when a move is done
-        if (getTurn() == BLACK)
-        {
-            setTurn(WHITE);
-        }
-        else
-        {
-            setTurn(BLACK);
-        }
-        marker();
+        switchTurn();
         // in case there is no legal move for the next user the side is switched
         if (!hasTileToFlip())
         {
-            if (getTurn() == BLACK)
-            {
-                setTurn(WHITE);
-            }
-            else
-            {
-                setTurn(BLACK);
-            }
-            // updating the legal tiles after changing the side
-            marker();
+            switchTurn();
         }
     }
     // reconstructing the board

b61c908fcfd875b2c7aadd9c8e0e1d3ca1c6d7a4
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-14 21:07:22 +0300

    Update flip_tiles.cpp

--- flip_tiles.cpp
+++ flip_tiles.cpp
@@ -9,7 +9,7 @@
  */
 void Table::flipTiles(coor move)
 {
-    coor c, temp;
+    coor curr, toFlip;
     bool isFlipped = false;
     // valid move guard in case there is a problem with the isLegal function
     bool isValid = false;
@@ -21,68 +21,70 @@ void Table::flipTiles(coor move)
         for (int i = 1;; i++)
         {
             // multiplying by 'i' to move along the directions
-            c.row = move.row + dir[0] * i;
-            c.col = move.col + dir[1] * i;
+            curr.row = move.row + dir[0] * i;
+            curr.col = move.col + dir[1] * i;
 
             // if the square is not on the board, then it is not a legal move
-            if (!isOnBoard(c))
+            if (!isOnBoard(curr))
                 break;
 
             // if the square is empty, the goal is not reached
-            if (getBoard(c.row, c.col) == EMPTY || getBoard(c.row, c.col) == LEGAL)
+            if (getBoard(curr.row, curr.col) == EMPTY || getBoard(curr.row, curr.col) == LEGAL)
                 break;
 
             // isFlipped changes to true if there is a tile to flip
             // when the player's square is found, there must be at least one tile to flip
-            if (getBoard(c.row, c.col) == getOpponent())
+            if (getBoard(curr.row, curr.col) == getOpponent())
                 isFlipped = true;
 
             // when a square in the same color as players is found
-            if (getBoard(c.row, c.col) == getTurn())
+            if (getBoard(curr.row, curr.col) == getTurn())
             {
                 // if isFlipped is not true, then it is not a legal move
                 if (!isFlipped)
                     break;
                 isValid = true;
 
-                // temp.row and temp.col are assigned the number along the legal direction
-                temp.row = move.row + dir[0];
-                temp.col = move.col + dir[1];
+                // toFlip is assigned the numbers along the legal direction
+                // to flip the tiles between curr and move, not including curr and move
+                // [curr]-----[toFlip]-----[move]
+                toFlip.row = move.row + dir[0];
+                toFlip.col = move.col + dir[1];
 
                 // assigning the squares, when the direction is horizontal
                 if (dir[0] == 0)
                 {
-                    // assign till c.col
-                    while (temp.col != c.col)
+                    // assign till curr.col
+                    while (toFlip.col != curr.col)
                     {
                         // incrase the number by the delta
-                        setBoard(temp.row, temp.col, getTurn());
-                        temp.col += dir[1];
+                        setBoard(toFlip.row, toFlip.col, getTurn());
+                        toFlip.col += dir[1];
                     }
                 }
 
                 // assigning the squares, when the direction is vertical
                 else if (dir[1] == 0)
                 {
-                    // assign till c.row
-                    while (temp.row != c.row)
+                    // assign till curr.row
+                    while (toFlip.row != curr.row)
                     {
                         // incrase the number by the delta
-                        setBoard(temp.row, temp.col, getTurn());
-                        temp.row += dir[0];
+                        setBoard(toFlip.row, toFlip.col, getTurn());
+                        toFlip.row += dir[0];
                     }
                 }
 
                 // assigning the squares, when the direction is cross
                 else
                 {
-                    // assign till c.row and c.col
-                    while (temp.row != c.row && temp.col != c.col)
+                    // assign till curr.row and curr.col
+                    while (toFlip.row != curr.row && toFlip.col != curr.col)
                     {
                         // incrase the numbers by the delta
-                        setBoard(temp.row, temp.col, getTurn());
-                        temp.row += dir[0];
-                        temp.col += dir[1];
+                        setBoard(toFlip.row, toFlip.col, getTurn());
+                        toFlip.row += dir[0];
+                        toFlip.col += dir[1];
                     }
                 }
                 break;

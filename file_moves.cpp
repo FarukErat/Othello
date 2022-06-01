493f5dc4aa99337fb8f6cb22421448a77e44fc90
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-06-02 01:30:34 +0300

    Update file_moves.cpp

--- file_moves.cpp
+++ file_moves.cpp
@@ -2,24 +2,33 @@
 
 #include "table.hpp"
 
+/**
+ * @brief returns moves from a file in order whenever called
+ *
+ * @param none
+ *
+ * @return coordinates in form of coor
+ */
 coor Table::fileMoves()
 {
 	// returns moves from files
 	return moves[moveCount++];
 }
+
 /**
  * @brief the parameter delay, delays in miliseconds
+ *
+ * @param delayInMs
  * 
- * @param delayInMs 
- * @return coor 
+ * @return coordinates in form of coor
  */
 coor Table::filePlays(unsigned delayInMs)
 {
 	coor c = fileMoves();
-    sleep(delayInMs); // delays in second
+	sleep(delayInMs); // delays in second
 	// for a better game exp, prints cpu's moves
 	cout << "row: " << moves[moveCount].row + 1 << endl;
 	cout << "col: " << moves[moveCount].col + 1 << endl;
 	sleep(delayInMs); // delays in second
-    return c;
+	return c;
 }

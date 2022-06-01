f0c4eff53ab16ee40f13c014b71183af36ff214c
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-06-02 01:27:39 +0300

    Update check_file.cpp

--- check_file.cpp
+++ check_file.cpp
@@ -2,6 +2,14 @@
 
 #include "table.hpp"
 
+/**
+ * @brief pre-plays the moves from a file to see if the file has valid moves
+ *
+ * @param none
+ *
+ * @return true
+ * @return false
+ */
 bool Table::checkFile()
 {
     // Check if the file is empty
